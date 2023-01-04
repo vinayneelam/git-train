6381654a8821bc877e918e9f78c7c530d5063155
Vinay Seshu Neelam <vinayneelam15@gmai.com>
2023-01-04 15:00:34 +0530

    first commit

---
+++ calc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def mod():
+    return ("hello world")
+
